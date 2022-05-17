a7869888b04a213e9f38a5ed82e1bda347fd3fea
任柏庆0928 <65744293+RenBoQing@users.noreply.github.com>
2022-05-17 13:58:59 +0800

    Update IndexController.java

--- src/main/java/com/rbq/mybatisplus/controller/IndexController.java
+++ src/main/java/com/rbq/mybatisplus/controller/IndexController.java
@@ -16,6 +16,7 @@ public class IndexController {
         System.out.println("测试数据测试仪");
         System.out.println("测试数据很好言");
         System.out.println("嘿嘿额年底为代价我去");
+        System.out.println("远程修改了代码");
         return "Hello World";
     }
 }
