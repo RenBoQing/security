fb24ee7be163d94939e891797058d5f7b039675c
RenBoQing <2098510757@qq.com>
2022-05-17 13:52:51 +0800

    mybatis-mybatis-plus 提交代码    matser 测试数据

--- src/main/java/com/rbq/mybatisplus/controller/IndexController.java
+++ src/main/java/com/rbq/mybatisplus/controller/IndexController.java
@@ -15,6 +15,7 @@ public class IndexController {
         System.out.println("测试数据");
         System.out.println("测试数据测试仪");
         System.out.println("测试数据很好言");
+        System.out.println("嘿嘿额年底为代价我去");
         return "Hello World";
     }
 }
